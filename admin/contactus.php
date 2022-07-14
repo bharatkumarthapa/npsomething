1cf94dc44d01df03fc4384808e709009a3094602
Bharat <77001005+bharatkumarthapa@users.noreply.github.com>
2022-07-14 22:48:14 +0545

    Update contactus.php

--- admin/contactus.php
+++ admin/contactus.php
@@ -32,9 +32,9 @@ $error="Something went wrong . Please try again.";
         <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
         <meta name="author" content="Coderthemes">
 
-        <!-- App favicon -->
+    
         <link rel="shortcut icon" href="assets/images/favicon.ico">
-        <!-- App title -->
+        
         <title>Contact us Page</title>
 
         <!-- App css -->
