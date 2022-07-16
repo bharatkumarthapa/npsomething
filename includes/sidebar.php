03c6e5ff8dc8b73639c02f50e1853389a869c9c7
Bharat <77001005+bharatkumarthapa@users.noreply.github.com>
2022-07-16 22:01:08 +0545

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,7 +16,7 @@
       </div>
   </div>
 
-          <!-- Categories Widget -->
+          
           <div class="card my-4">
             <h5 class="card-header">Categories</h5>
             <div class="card-body">
