c26e58f535e836d82cc0c76dd5666d0e913ba209
Bharat <77001005+bharatkumarthapa@users.noreply.github.com>
2022-07-16 22:02:06 +0545

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,5 @@
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">&copy;<?php echo date('Y');?> smscvs.exe
-        </p>
+        <p class="m-0 text-center text-white">&copy;<?php echo date('Y');?> smscvs.exe </p>
       </div>
-      
     </footer>
